500271c8336d316f98720a5ca97e1d8f2ca2f57b
eg0rmeister <karpukhin.ea@phystech.edu>
2024-11-09 19:27:10 +0300

    made user input in main prettier and added an example input

--- main.cpp
+++ main.cpp
@@ -3,9 +3,30 @@
 
 int main(){
   Automaton automaton = UserInput::InputAutomaton();
-
-  do {
-    std::cout << automaton << std::endl;
-  } while (automaton.ApplyAllRules());
+  while(true){
+    std::cout << "Current state: " << automaton << std::endl;
+    std::cout << "Enter action to continue (n - next iteration, q - quit): ";
+    char answer;
+    std::cin >> answer;
+    std::cout << std::endl;
+    if (answer == 'q' || answer == 'Q'){
+      std::cout << "Quitting by user command" << std::endl;
+      break;
+    }
+    if (answer != 'n' && answer != 'N'){
+      std::cout << "Incorrect input" << std::endl;
+      continue;
+    }
+    // answer is 'n' or 'N'
+    char automaton_result = automaton.ApplyFirstRule();
+    if (automaton_result == 2){
+      std::cout << "Automaton stopped execution" << std::endl;
+      break;
+    }
+    if (automaton_result == 1){
+      std::cout << "Automaton's state did not change" << std::endl;
+      break;
+    }
+  }
   return 0;
 }
